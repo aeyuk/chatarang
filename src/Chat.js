152dc46d7ca5945a267e41ae2867655d6a06e103
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-12 14:15:29 -0500

    Add Message, MessageForm

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
 import ChatHeader from './ChatHeader'
+import MessageList from './MessageList'
+import MessageForm from './MessageForm'
+
 class Chat extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            messages: [
+                {
+                    id: 1,
+                    userName: 'stephen',
+                    body: 'cool',
+                },
+                {
+                    id: 2,
+                    userName: 'dpalazzo',
+                    body: 'This guy is so annoying. I hate my job.',
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div className="Chat">
             <ChatHeader/>
+            <MessageList messages={this.state.messages}/>
+            <MessageForm/>
             </div>
         )
     }
