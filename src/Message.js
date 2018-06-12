c72d1a022bdca558bc8d107e28cf3dfb48fb3d04
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-12 16:05:08 -0500

    Style messages

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,49 @@ import React from 'react'
 
 const Message = (props) => {
     return (
-        <div className="Message">
-            {props.message.userName}: {props.message.body}
+        <div 
+            className="Message"
+            style={styles.message}
+        >
+            <div 
+                className="user"
+                style={styles.user}
+            >{props.message.userName}
+            </div>
+            <div 
+                className="time"
+                style={styles.time}
+            >1:10 PM
+            </div>
+            <div 
+                className="body"
+                style={styles.body}
+            >{props.message.body}
+            </div>
         </div>
     )
+}
+
+const styles = {
+    message: {
+        display: 'flex',
+        alignItems: 'baseline',
+        marginTop: '1rem',
+        padding: '0 1rem',
+    },
+    user: {
+        fontWeight: 'bold',
+        marginRight: '0.5rem',
+    },
+    time: {
+        color: '#999',
+        fontSize: '0.8rem',
+    },
+    body: {
+        flex: '1',
+        paddingLeft: '0.5rem',
+        
+    }
 }
 
 export default Message
