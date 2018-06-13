7f4d2c743a1d70d42e721fa8f501ebf6fdcf7898
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-13 13:23:54 -0500

    Add RoomList Component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-import RoomList from './RoomList'
 import UserInfo from './UserInfo'
+import RoomList from './RoomList'
 
 const Sidebar = (props) => {
   return (
-    <aside className="Sidebar" style={styles.sidebar}>
+    <aside
+      className="Sidebar"
+      style={styles.sidebar}
+    >
       <UserInfo user={props.user} />
       <h1
         style={{
@@ -15,9 +18,7 @@ const Sidebar = (props) => {
       >
         XTBC 18
       </h1>
-
-      <RoomList/>
-
+      <RoomList />
     </aside>
   )
 }
@@ -36,30 +37,11 @@ const styles = {
     padding: '0 1rem',
   },
 
-  h2: {
+  h1: {
     color: 'white',
     fontSize: '1.2rem',
-    marginTop: '0',
+    marginTop: 0,
   },
-
-  avatar: {
-    background: 'url(https://api.adorable.io/avatars/32/davey@getfretless.com)',
-    height: '40px',
-    width: '40px',
-    fontSize: '1rem',
-    borderRadius: '20px',
-    marginRight: '0.5rem'
-  },
-
-  userInfo: {
-      marginBottom: '1rem',
-      display: 'flex',
-      alignItems: 'center',
-  },
-
-  user: {
-      flex: '1',
-  }
 }
 
 export default Sidebar
