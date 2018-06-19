56267b859a5896a39c4dd097efe5023960b4131b
Cate Yuk <aeyuk@crimson.ua.edu>
2018-06-19 14:45:44 -0500

    Add a list of users in state

--- src/App.js
+++ src/App.js
@@ -2,55 +2,72 @@ import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
 import './App.css'
-import { auth } from './base'
+import base, { auth } from './base'
 import SignIn from './SignIn'
 import Main from './Main'
 
 class App extends Component {
-  state = {
-    user: {},
+  constructor() {
+    super()
+
+    const user = JSON.parse(localStorage.getItem('user')) || {}
+    this.state = {
+      user,
+      users: {},
+    }
   }
 
-  componentWillMount() {
-    const user = JSON.parse(localStorage.getItem('user'))
-
-    if (user) {
-      this.setState({ user })
-    }
+  componentDidMount() {
+    base.syncState(
+      'users',
+      {
+        context: this,
+        state: 'users',
+      }
+    )
 
     auth.onAuthStateChanged(
       user => {
         if (user) {
+          // we signed in
           this.handleAuth(user)
         } else {
+          // we signed out
           this.handleUnauth()
         }
       }
     )
   }
 
-  handleAuth = (oauthUser) => {
-    const user = {
-      email: oauthUser.email,
-      uid: oauthUser.uid,
-      displayName: oauthUser.displayName,
-      photoUrl: oauthUser.photoURL
-    }
-    this.setState({ user })
-    localStorage.setItem('user', JSON.stringify(user))
-  }
-
   signedIn = () => {
     return this.state.user.uid
   }
 
-  signOut = () => {
-    auth.signOut()
+  handleAuth = (oauthUser) => {
+    // Build the user object
+    const user = {
+      uid: oauthUser.uid,
+      displayName: oauthUser.displayName,
+      email: oauthUser.email,
+      photoUrl: oauthUser.photoURL,
+    }
+
+    // Update the list of users
+    const users = {...this.state.users}
+    users[user.uid] = user
+
+    // Update state and localStorage
+    this.setState({ user, users })
+    localStorage.setItem('user', JSON.stringify(user))
   }
 
   handleUnauth = () => {
     this.setState({ user: {} })
     localStorage.removeItem('user')
+  }
+
+  signOut = () => {
+    auth.signOut()
   }
 
   render() {
@@ -59,7 +76,7 @@ class App extends Component {
         <Switch>
           <Route
             path="/sign-in"
-            render={navProps => (
+            render={() => (
               this.signedIn()
                 ? <Redirect to="/rooms/general" />
                 : <SignIn />
@@ -69,12 +86,12 @@ class App extends Component {
             path="/rooms/:roomName"
             render={navProps => (
               this.signedIn()
-              ? <Main
-                  user={this.state.user}
-                  signOut={this.signOut}
-                  {...navProps}
-                />
-              : <Redirect to="/sign-in" />
+                ? <Main
+                    user={this.state.user}
+                    signOut={this.signOut}
+                    {...navProps}
+                  />
+                : <Redirect to="/sign-in" />
             )}
           />
           <Route
